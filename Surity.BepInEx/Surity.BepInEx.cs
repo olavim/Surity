999e7ef06e138c279265c52233dabf585c71bef6
Olavi Mustanoja <tilastokeskus@gmail.com>
2023-03-08 16:41:42 +0200

    chore: simplify project files

--- Surity.BepInEx/Surity.BepInEx.cs
+++ Surity.BepInEx/Surity.BepInEx.cs
@@ -3,11 +3,11 @@
 namespace Surity
 {
 	[BepInPlugin(ModId, ModName, ModVersion)]
-	public class SurityPlugin : BaseUnityPlugin
+	public sealed class SurityPlugin : BaseUnityPlugin
 	{
 		public const string ModId = "io.olavim.surity.bepinex";
 		public const string ModName = "Surity.BepInEx";
-		public const string ModVersion = "1.0.0";
+		public const string ModVersion = ThisAssembly.Project.Version;
 
 		private void Start()
 		{
